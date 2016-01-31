a360db6614d866397bf700a474daceb96e81abb3
Jason Freidman <jason@periscope.io>
2016-01-30 16:21:19 -0800

    New main -- Adds in room 3 bots and better tower controls

--- dist/main.js
+++ dist/main.js
@@ -1,4 +1,4 @@
-var Agent, Builder, Cell, Config, Deliverator, Guard, Healbot, HunterKiller, MegaMiner, Mine, PathUtils, Upgrader, a, cell, creep, e, harvestOnly, mines, name, nearestTarget, primaryRoom, primarySpawn, primaryTower, ref, room2, room2mines, targetCounts, upgraders;
+var Agent, Builder, Cell, Config, Deliverator, Guard, Healbot, HunterKiller, MegaMiner, Mine, PathUtils, Upgrader, _, a, cell, creep, creepByJob, e, harvestOnly, k, len, mines, name, nearestTarget, primaryRoom, primarySpawn, primaryTower, ref, ref1, ref2, ref3, role, room2, room2Pos, room2mines, room3, room3Pos, targetCounts, upgraders;
 
 Agent = require('agent');
 
@@ -24,6 +24,19 @@ Healbot = require('healbot');
 
 HunterKiller = require('hunter_killer');
 
+Array.prototype.shuffle = function() {
+  var a, i, j, t;
+  a = this;
+  i = a.length;
+  while (--i > 0) {
+    j = ~~(Math.random() * (i + 1));
+    t = a[j];
+    a[j] = a[i];
+    a[i] = t;
+  }
+  return a;
+};
+
 Array.prototype.sum = function() {
   if (!this.length) {
     return 0;
@@ -39,27 +52,39 @@ primaryRoom = primarySpawn.room;
 
 room2 = Game.flags.Room2.room;
 
+room2Pos = Game.flags.Room2.pos;
+
+room3 = Game.flags.Room3.room;
+
+room3Pos = Game.flags.Room3.pos;
+
 primaryTower = primaryRoom.find(FIND_MY_STRUCTURES).filter(function(s) {
   return s.structureType === 'tower';
 })[0];
 
 targetCounts = {
-  source1: 2,
-  source2: 2,
+  source1: 0,
+  source2: 0,
   tower_filler: 1,
-  transporter: 6,
-  mega_miner: 2,
-  room2_mega_miner: 1,
-  room2_mega_miner2: 1,
-  room2_transporter: 8,
-  mega_miner2: 2,
+  mega_miner2: 1,
+  transporter: 5,
+  mega_miner: 1,
   repair: 2,
   builder: 1,
   upgrader: 3,
   upgrader_filler: 2,
   guard: 3,
   healbot: 2,
-  hunter_killer: 2
+  hunter_killer: 2,
+  healbot_2: 2,
+  hunter_killer_2: 2,
+  room2_mega_miner2: 1,
+  room2_transporter: 8,
+  room2_mega_miner: 1,
+  position_miner1: 1,
+  position_miner1_transport: 3,
+  position_miner2: 1,
+  position_miner2_transport: 3
 };
 
 try {
@@ -84,7 +109,7 @@ try {
       Game.notify("Failed to move flag!", 20);
     }
     try {
-      nearestTarget = new PathUtils(primaryTower).sortByDistance(primaryRoom.find(FIND_HOSTILE_CREEPS));
+      nearestTarget = new PathUtils(primaryTower).sortByDistance(primaryRoom.find(FIND_HOSTILE_CREEPS))[0];
       if (nearestTarget != null) {
         primaryTower.attack(nearestTarget);
       }
@@ -95,20 +120,29 @@ try {
     }
   } else {
     try {
-      if (Game.flags.HuntersMark.pos.roomName !== Game.flags.Room2.pos.roomName) {
-        Game.flags.HuntersMark.setPosition(Game.flags.Room2.pos);
+      if (Game.flags.HuntersMark.pos.roomName !== room2Pos.roomName) {
+        Game.flags.HuntersMark.setPosition(room2Pos);
       }
     } catch (_error) {
       e = _error;
-      throw e;
+      console.log("failed to move flag");
       Game.notify("Failed to move flag!", 20);
+      throw e;
+    }
+  }
+  if (primaryTower.energy > primaryTower.energyCapacity / 2) {
+    nearestTarget = new PathUtils(primaryTower).sortByDistance(primaryRoom.find(FIND_STRUCTURES).filter(function(s) {
+      return s.hits < Math.min(s.hitsMax, Config.MaxWallHP);
+    }))[0];
+    if (nearestTarget != null) {
+      primaryTower.repair(nearestTarget);
     }
   }
 } catch (_error) {
+  console.log("Caught exception! " + e);
   if (Config.ThrowExceptions) {
     throw e;
   }
-  console.log("Caught exception! " + e);
 }
 
 try {
@@ -135,26 +169,81 @@ upgraders = function() {
   return u[parseInt(Math.random() * u.length)];
 };
 
+creepByJob = {};
+
 ref = Game.creeps;
 for (name in ref) {
   creep = ref[name];
+  role = creep.memory['role'];
+  creepByJob[role] || (creepByJob[role] = []);
+  creepByJob[role].push(creep);
+}
+
+ref1 = ((function() {
+  var ref1, results;
+  ref1 = Game.creeps;
+  results = [];
+  for (_ in ref1) {
+    creep = ref1[_];
+    results.push(creep);
+  }
+  return results;
+})()).shuffle();
+for (k = 0, len = ref1.length; k < len; k++) {
+  creep = ref1[k];
+  if (Game.flags.ClearTargets != null) {
+    delete creep.memory.sourceTarget;
+    delete creep.memory.deliverTarget;
+  }
   try {
     switch (creep.memory.role.split(":")[0]) {
+      case 'position_miner1':
+        new PositionMiner(creep, (ref2 = Game.flags.Mine_3_0) != null ? ref2.pos : void 0).loop();
+        break;
+      case 'position_miner2':
+        new PositionMiner(creep, (ref3 = Game.flags.Mine_3_1) != null ? ref3.pos : void 0).loop();
+        break;
+      case 'position_miner1_transport':
+        new Deliverator(creep, (function() {
+          return creepByJob['position_miner1'][0];
+        }), (function() {
+          return primarySpawn;
+        })).loop();
+        break;
+      case 'position_miner2_transport':
+        new Deliverator(creep, (function() {
+          return creepByJob['position_miner2'][0];
+        }), (function() {
+          return primarySpawn;
+        })).loop();
+        break;
       case 'healbot':
         new Healbot(creep).loop(Game.flags.HuntersMark);
         break;
       case 'hunter_killer':
         new HunterKiller(creep).loop(Game.flags.HuntersMark);
         break;
+      case 'healbot_2':
+        new Healbot(creep).loop(Game.flags.HuntersMark2);
+        break;
+      case 'hunter_killer_2':
+        new HunterKiller(creep).loop(Game.flags.HuntersMark2);
+        break;
       case 'guard':
         new Guard(creep).loop();
         break;
       case 'tower_filler':
-        new Deliverator(creep, (function() {
-          return primarySpawn;
-        }), (function() {
-          return primaryTower;
-        })).loop();
+        if (primaryTower.energy < primaryTower.energyCapacity) {
+          new Deliverator(creep, (function() {
+            return primarySpawn;
+          }), (function() {
+            return primaryTower;
+          })).loop();
+        } else {
+          if (!Config.NoBuilders) {
+            new Builder(creep).loop();
+          }
+        }
         break;
       case 'upgrader_filler':
         new Deliverator(creep, (function() {
@@ -219,8 +308,8 @@ for (name in ref) {
         (!Config.NoRepairs ? new Deliverator(creep, (function() {
           return primarySpawn;
         }), (function() {
-          return (new PathUtils(creep)).sortByDistance(primaryRoom.find(FIND_STRUCTURES).filter(function(s) {
-            return s.hits < s.hitsMax;
+          return (new PathUtils(creep)).sortByDistance(primaryRoom.find(FIND_MY_STRUCTURES).filter(function(s) {
+            return s.structureType !== 'rampart' && s.hits < s.hitsMax;
           }))[0];
         })).loop() : void 0) || (!Config.NoBuilders ? new Builder(creep).loop() : void 0);
     }
--- src/main.coffee
+++ src/main.coffee
@@ -12,6 +12,16 @@ Healbot = require('healbot')
 HunterKiller = require('hunter_killer')
 
 
+Array.prototype.shuffle = ->
+    a = @
+    i = a.length
+    while --i > 0
+        j = ~~(Math.random() * (i + 1))
+        t = a[j]
+        a[j] = a[i]
+        a[i] = t
+    a
+
 Array.prototype.sum = ->
   return 0 unless @length
   @.reduce((a,b) -> a+b)
@@ -19,18 +29,19 @@ Array.prototype.sum = ->
 primarySpawn = Game.spawns.Spawn1
 primaryRoom = primarySpawn.room
 room2 = Game.flags.Room2.room
+room2Pos = Game.flags.Room2.pos
+room3 = Game.flags.Room3.room
+room3Pos = Game.flags.Room3.pos
+
 primaryTower = primaryRoom.find(FIND_MY_STRUCTURES).filter((s)->s.structureType == 'tower')[0]
 
 targetCounts = 
-  source1:2
-  source2: 2
+  source1: 0
+  source2: 0
   tower_filler: 1
-  transporter:6
-  mega_miner: 2
-  room2_mega_miner: 1
-  room2_mega_miner2: 1
-  room2_transporter: 8
-  mega_miner2: 2
+  mega_miner2: 1
+  transporter:5
+  mega_miner: 1
   repair: 2
   builder: 1
   upgrader: 3
@@ -38,6 +49,15 @@ targetCounts =
   guard: 3
   healbot: 2
   hunter_killer:2
+  healbot_2: 2
+  hunter_killer_2:2
+  room2_mega_miner2: 1
+  room2_transporter: 8
+  room2_mega_miner: 1
+  position_miner1:1
+  position_miner1_transport:3
+  position_miner2:1
+  position_miner2_transport:3
 
 try 
   if (1 for a of primaryRoom.find(FIND_HOSTILE_CREEPS)).length > 0
@@ -52,7 +72,7 @@ try
       Game.notify("Failed to move flag!", 20)
     
     try 
-      nearestTarget = new PathUtils(primaryTower).sortByDistance(primaryRoom.find(FIND_HOSTILE_CREEPS))
+      nearestTarget = new PathUtils(primaryTower).sortByDistance(primaryRoom.find(FIND_HOSTILE_CREEPS))[0]
       primaryTower.attack(nearestTarget) if nearestTarget?
     catch e
       console.log("Failed to use tower!!!")
@@ -60,14 +80,20 @@ try
   else
     # PEACE
     try
-      if Game.flags.HuntersMark.pos.roomName != Game.flags.Room2.pos.roomName
-        Game.flags.HuntersMark.setPosition(Game.flags.Room2.pos)
+      if Game.flags.HuntersMark.pos.roomName != room2Pos.roomName
+        Game.flags.HuntersMark.setPosition(room2Pos)
     catch e
-      throw e
+      console.log("failed to move flag")
       Game.notify("Failed to move flag!", 20)
+      throw e
+
+  if primaryTower.energy > primaryTower.energyCapacity / 2
+    nearestTarget = new PathUtils(primaryTower).sortByDistance(primaryRoom.find(FIND_STRUCTURES).filter((s)-> s.hits < Math.min(s.hitsMax, Config.MaxWallHP)))[0]
+    primaryTower.repair(nearestTarget) if nearestTarget?
+
 catch
-  throw e if Config.ThrowExceptions
   console.log("Caught exception! #{e}")
+  throw e if Config.ThrowExceptions
 
 try
   cell = new Cell(primaryRoom, targetCounts)
@@ -100,12 +126,27 @@ for creep in (creep for _, creep of Game.creeps).shuffle()
   if Game.flags.ClearTargets?
     delete creep.memory.sourceTarget
     delete creep.memory.deliverTarget
+  #console.log 'run', creep.name
   try
     switch creep.memory.role.split(":")[0]
+      when 'position_miner1' then new PositionMiner(creep, Game.flags.Mine_3_0?.pos).loop()
+      when 'position_miner2' then new PositionMiner(creep, Game.flags.Mine_3_1?.pos).loop()
+
+      when 'position_miner1_transport' then new Deliverator(creep, (-> creepByJob['position_miner1'][0]), (-> primarySpawn)).loop()
+      when 'position_miner2_transport' then new Deliverator(creep, (-> creepByJob['position_miner2'][0]), (-> primarySpawn)).loop()
+
       when 'healbot' then new Healbot(creep).loop(Game.flags.HuntersMark)
       when 'hunter_killer' then new HunterKiller(creep).loop(Game.flags.HuntersMark)
+
+      when 'healbot_2' then new Healbot(creep).loop(Game.flags.HuntersMark2)
+      when 'hunter_killer_2' then new HunterKiller(creep).loop(Game.flags.HuntersMark2)
+
       when 'guard' then new Guard(creep).loop()
-      when 'tower_filler' then new Deliverator(creep, (-> primarySpawn), (-> primaryTower )).loop()
+      when 'tower_filler'  
+        if primaryTower.energy < primaryTower.energyCapacity
+          new Deliverator(creep, (-> primarySpawn), (-> primaryTower )).loop()
+        else
+          new Builder(creep).loop() unless Config.NoBuilders
       when 'upgrader_filler' then new Deliverator(creep, (-> primarySpawn), ( upgraders )).loop()
       when 'mega_miner' then new MegaMiner(creep, mines[0].source).loop()
       when 'mega_miner2' then new MegaMiner(creep, mines[1].source).loop()
@@ -121,7 +162,7 @@ for creep in (creep for _, creep of Game.creeps).shuffle()
       when 'repair'
         ((new Deliverator(creep, 
           (-> primarySpawn), 
-          (-> (new PathUtils(creep)).sortByDistance(primaryRoom.find(FIND_STRUCTURES).filter((s)->s.hits < s.hitsMax ))[0])).loop() unless Config.NoRepairs  ) || 
+          (-> (new PathUtils(creep)).sortByDistance(primaryRoom.find(FIND_MY_STRUCTURES).filter((s)->s.structureType != 'rampart' && s.hits < s.hitsMax ))[0])).loop() unless Config.NoRepairs  ) || 
          (new Builder(creep).loop() unless Config.NoBuilders))
   catch e
     throw e if Config.ThrowExceptions
